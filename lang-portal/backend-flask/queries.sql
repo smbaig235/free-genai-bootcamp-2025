13f8933d4cffd3709c2d71e91b6ca799107dc66d
smbaig235 <smbaig251@gmail.com>
2025-02-13 00:19:29 -0800

    Insert vocabulary words in database

---
+++ lang-portal/backend-flask/queries.sql
@@ -0,0 +1,11 @@
+-- First insert the word
+INSERT INTO words (french, english, parts) 
+VALUES ('oeuf', 'egg', 'noun');
+
+-- Then insert the word group
+
+
+-- Then insert the word group association
+
+
+-- Then insert the word group association
